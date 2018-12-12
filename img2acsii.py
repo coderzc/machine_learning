6a03898d4235fbb32adaec7e1aa531f6c36ec246
coderzc <zc1217zc@126.com>
2018-12-12 15:53:54 +0800

    优化img2acsii

--- img2acsii.py
+++ img2acsii.py
@@ -1,23 +1,45 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import argparse
-import math
+import matplotlib.pyplot as plt
 
 '''
 把图片转化为acsii字符
 '''
 
 
-# 将256灰度映射到字符上
+# 将256灰度映射到字符集上
 def get_char(r, g, b, alpha=256):
     if alpha == 0:
         return ' '
     # 字符库
     ascii_char = list("   ...',;:clodxkO0KXNWMMMM")
     # ascii_char=list("##XXxxx+++===---;;,,...  ")
-    gray = int((19595 * r + 38469 * g + 7474 * b) >> 16)
+    # ascii_char=list(' .:-=+*#%@')
+    gray = int((19595 * r + 38469 * g + 7472 * b) >> 16)
     length = len(ascii_char)
-    unit = (256) / length
+    unit = (256.0 + 1) / length
     return ascii_char[int(gray / unit)]
+
+
+# 使用plt 显示图片
+def show_img(img):
+    plt.figure("Test")
+    plt.title('Test image')
+    plt.axis('off')  # 不显示坐标轴
+    plt.imshow(img)
+    plt.show()
+
+
+# 输出到图像
+def print_img(txt):
+    # 计算ascii图事迹大小
+    img_new = Image.new('RGB', (0, 0), (0, 0, 0))
+    width, height = ImageDraw.Draw(img_new).textsize(txt)
+
+    img_new = Image.new('RGB', (width, height), (0, 0, 0))
+    d = ImageDraw.Draw(img_new)
+    d.text((0, 0), txt, fill=(255, 255, 255))
+    show_img(img_new)
 
 
 if __name__ == '__main__':
@@ -38,11 +60,11 @@ if __name__ == '__main__':
     # 打开图片
     img = Image.open(IMG)
 
-    # 压缩
+    # 压缩,调整图像比例
     w, h = img.size
     h_ = 0
     if HEIGHT == 0:
-        h_ = int(((WIDTH * h) / w) * 0.7)
+        h_ = int(((WIDTH * h) / w) * 0.415)
     else:
         h_ = HEIGHT
     img = img.resize((WIDTH, h_), Image.NEAREST)
@@ -54,12 +76,16 @@ if __name__ == '__main__':
 
     for i in range(img.size[1]):
         for j in range(img.size[0]):
-            txt += get_char(*rgba_img.getpixel((j, i))) + ''
+            # txt += '\033[34m' + get_char(*rgba_img.getpixel((j, i))) + '\033[0m' # 蓝色字符
+            txt += get_char(*rgba_img.getpixel((j, i)))
         txt += '\n'
 
-    print(txt)
+    # print(txt)
 
     # 字符画输出到文件
     if OUTPUT:
         with open(OUTPUT, 'w') as f:
             f.write(txt)
+
+    # 输出到图像
+    print_img(txt)
