6219cc2ee71c2c49a13469478529532c2b681be9
coderzc <zc1217zc@126.com>
2020-06-15 23:48:36 +0800

    2019-ncov

--- 2019-nCov.py
+++ 2019-nCov.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 """
 拟合2019-nCov肺炎感染确诊人数
-https://blog.csdn.net/weixin_36474809/article/details/104101055
+https://blog.csdn.net/z_ccsdn/article/details/104134358
 """
 import datetime
 
@@ -20,10 +20,43 @@ sdate = None
 hyperparameters_r = None
 hyperparameters_K = None
 
+'''
+从csv获取数据
+csv 样例：
+01.11 41 0
+01.12 41 0
+'''
+
+
+def load_data_fromcsv(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            if line:
+                line_array = line.split(' ')
+                if len(line_array) == 3:
+                    data.append(
+                        {'date': line_array[0], 'confirm': line_array[1], 'suspect': line_array[2]})
+    data.sort(key=lambda x: x["date"])
+    date_temple = '%Y.%m.%d'
+    # 获取首次出现感染人数的日期
+    global sdate
+    sdate = datetime.datetime.strptime(
+        '2020.' + data[0]['date'], f'{date_temple}').date()
+    x_data_history = [datetime.datetime.strptime('2020.' + dd['date'], f'{date_temple}').date().strftime("%m-%d") for dd
+                      in
+                      data]
+    t = [datetime.datetime.strptime(
+        '2020.' + dd['date'], f'{date_temple}').date() for dd in data]
+    P_confirm = [int(dd['confirm']) for dd in data]
+    P_suspect = [int(dd['suspect']) for dd in data]
+    return np.array(t, dtype=np.datetime64), np.array(P_confirm), np.array(P_suspect), x_data_history
 
 def load_data():
     # 拉取腾讯新闻数据
-    res = requests.get('http://service-n9zsbooc-1252957949.gz.apigw.tencentcs.com/release/qq')
+    res = requests.get('https://service-n9zsbooc-1252957949.gz.apigw.tencentcs.com/release/qq')
     res_json = res.json()
     data = res_json['data']['wuwei_ww_cn_day_counts']
 
@@ -32,8 +65,7 @@ def load_data():
     data.append({'date': '01.12', 'confirm': '41', 'suspect': '0'})
     data.sort(key=lambda x: x["date"])
 
-    # 去掉最后一天(因为今天的数据不准要等，明天早上才会有准确的数据)
-    # 因为20号以前并非是全国数据，数据不好要去掉
+    # 因为21号以前并非是全国数据，数据不好要去掉
     data = data[10:]
     print(data)
     # 获取首次出现感染人数的日期
@@ -56,9 +88,9 @@ def day_delay(t):
     return days
 
 
-def logistic_increase_function(t, K, P0):
+def logistic_increase_function(t, P0):
     r = hyperparameters_r
-    # K = hyperparameters_K
+    K = hyperparameters_K
     # t:time   t0:initial time    P0:initial_value    K:capacity  r:increase_rate
     exp_value = np.exp(r * (t))
     return (K * exp_value * P0) / (K + (exp_value - 1) * P0)
@@ -67,6 +99,7 @@ def logistic_increase_function(t, K, P0):
 if __name__ == '__main__':
     #  日期及感染人数
     t, P_confirm, P_suspect, x_show_data = load_data()
+    # t, P_confirm, P_suspect, x_show_data = load_data_fromcsv('~/data.csv')
     x_data, y_data = day_delay(t), P_confirm
 
     # 分隔训练测试集,将最后的30%数据作为测试集
@@ -84,25 +117,26 @@ if __name__ == '__main__':
     mse = float("inf")
     r = None
     k = None
-    # 网格搜索优化r和K参数
-    # for k_ in np.arange(44990, 50000, 1):
-    #     hyperparameters_K = k_
-    for r_ in np.arange(0, 1, 0.01):
-        # 用最小二乘法估计拟合
-        hyperparameters_r = r_
-        popt_, pcov_ = curve_fit(logistic_increase_function, x_train, y_train)
-        # # 获取popt里面是拟合系数
-        print("K:capacity  P0:initial_value   r:increase_rate")
-        # print(k_, popt_, r_)
+    # 网格搜索来优化r和K参数
+    max_k = 50000  # 限定的最大感染人数
+    for k_ in np.arange(20000, max_k, 1):
+        hyperparameters_K = k_
+        for r_ in np.arange(0, 1, 0.01):
+            # 用最小二乘法估计拟合
+            hyperparameters_r = r_
+            popt_, pcov_ = curve_fit(logistic_increase_function, x_train, y_train)
+            # # 获取popt里面是拟合系数
+            print("K:capacity  P0:initial_value   r:increase_rate")
+            print(k_, popt_, r_)
 
-        # 计算均方误差对测试集进行验证
-        mse_ = mean_squared_error(y_test, logistic_increase_function(x_test, *popt_))
-        print("mse:", mse_)
-        if mse_ <= mse:
-            mse = mse_
-            popt = popt_
-            r = r_
-            # k = k_
+            # 计算均方误差对测试集进行验证
+            mse_ = mean_squared_error(y_test, logistic_increase_function(x_test, *popt_))
+            print("mse:", mse_)
+            if mse_ <= mse:
+                mse = mse_
+                popt = popt_
+                r = r_
+                k = k_
     hyperparameters_K = k
     hyperparameters_r = r
     print("----------------")
@@ -112,25 +146,17 @@ if __name__ == '__main__':
     popt, pcov = curve_fit(logistic_increase_function, x_data, y_data)
     print("K:capacity  P0:initial_value   r:increase_rate")
     print(hyperparameters_K, popt, hyperparameters_r)
-    # y_data_suspect = P_suspect
-    # popt_suspect, pcov_suspect = curve_fit(logistic_increase_function, x_data, y_data_suspect)
-    # # # 获取popt里面是拟合系数
-    # print("K:capacity  P0:initial_value   r:increase_rate")
-    # print(popt_suspect)
 
     # 未来预测
-    future = np.linspace(0, 35, 35)
+    date_nums = 32  # 需要预测的总天数，从第一天开始算起
+    future = np.linspace(0, date_nums, date_nums)
     future = np.array(future)
     future_predict = logistic_increase_function(future, *popt)
-    # future_predict_suspect = logistic_increase_function(future, *popt_suspect)
 
     # 绘图
     x_show_data_all = [(sdate + (datetime.timedelta(days=fu))).strftime("%m-%d") for fu in future]
     plt.scatter(x_show_data, P_confirm, s=35, c='green', marker='.', label="确诊人数")
-    # plt.scatter(x_show_data, P_suspect, s=30, c='orange', marker='.', label="疑似人数")
     plt.plot(x_show_data_all, future_predict, 'r-s', marker='+', linewidth=1.5, label='预测曲线')
-    # plt.plot(x_show_data_all, future_predict_suspect, 'b-s', color='steelblue', marker='*', linewidth=1.0,
-    #          label='预测疑似曲线')
 
     plt.tick_params(labelsize=5)
     plt.xlabel('时间', FontProperties=font)
